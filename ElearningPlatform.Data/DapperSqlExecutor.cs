0dd9d00a20b74995b846a8716f46f822df4e1b10
Jakub Hypki <37691329+jhypki@users.noreply.github.com>
2025-03-30 18:50:49 +0200

    chore: refactor dapperSqlExecutor class

--- ElearningPlatform.Data/DapperSqlExecutor.cs
+++ ElearningPlatform.Data/DapperSqlExecutor.cs
@@ -1,30 +1,27 @@
-using System.Data;
-using Dapper;
-using ElearningPlatform.Core.Interfaces.Data;
-
 namespace ElearningPlatform.Data;
+
+using System.Data;
+using Core.Interfaces.Data;
+using Dapper;
 
 public class DapperSqlExecutor<T>(IConnectionManager<T> connectionManager) : ISqlExecutor
     where T : class, IDbConnection
 {
-    private readonly IConnectionManager<T> _connectionManager =
-        connectionManager ?? throw new ArgumentNullException(nameof(connectionManager));
-
     public async Task<T1?> QueryFirstOrDefaultAsync<T1>(string sql, object? parameters = null)
     {
-        using var connection = _connectionManager.GetConnection();
+        using var connection = connectionManager.GetConnection();
         return await connection.QueryFirstOrDefaultAsync<T1>(sql, parameters);
     }
 
     public async Task<IEnumerable<T1>> QueryAsync<T1>(string sql, object? parameters = null)
     {
-        using var connection = _connectionManager.GetConnection();
+        using var connection = connectionManager.GetConnection();
         return await connection.QueryAsync<T1>(sql, parameters);
     }
 
     public async Task<int> ExecuteAsync(string sql, object? parameters = null)
     {
-        using var connection = _connectionManager.GetConnection();
+        using var connection = connectionManager.GetConnection();
         return await connection.ExecuteAsync(sql, parameters);
     }
 }
