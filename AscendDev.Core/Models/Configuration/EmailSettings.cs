d3aed84161e9fad44635a33fbd36110a47e4f0ed
Jakub Hypki <kubahypki7@gmail.com>
2025-10-15 18:42:02 +0200

    fix name conflict

--- AscendDev.Core/Models/Configuration/EmailSettings.cs
+++ AscendDev.Core/Models/Configuration/EmailSettings.cs
@@ -2,11 +2,13 @@ namespace AscendDev.Core.Models.Configuration;
 
 public class EmailSettings
 {
+    public const string SectionName = "Email";
+
     public string SmtpHost { get; set; } = string.Empty;
-    public int SmtpPort { get; set; }
-    public bool EnableSsl { get; set; }
-    public string Username { get; set; } = string.Empty;
-    public string Password { get; set; } = string.Empty;
+    public int SmtpPort { get; set; } = 587;
+    public string SmtpUsername { get; set; } = string.Empty;
+    public string SmtpPassword { get; set; } = string.Empty;
     public string FromEmail { get; set; } = string.Empty;
     public string FromName { get; set; } = string.Empty;
+    public bool EnableSsl { get; set; } = true;
 }
